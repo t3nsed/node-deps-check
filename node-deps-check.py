ba8ea16e0f51a24e6ff57325c16880d181d6bf52
Johann Hipp <johann.hipp@student.uni-halle.de>
2022-10-06 17:25:06 +0200

    added license counter

--- node-deps-check.py
+++ node-deps-check.py
@@ -1,10 +1,10 @@
-import os
-import sys
-import json
+import collections, os, sys, json
 
 output = 'node-deps-verbose.json'
+outputLicenseSums = 'node-deps-licenses.json'
 traverseDir = os.getcwd()
 data = {}
+licenseSums = []
 
 if len(sys.argv) > 1:
     traverseDir = sys.argv[1]
@@ -14,17 +14,33 @@ for root, dirs, files in os.walk(traverseDir):
     # if 'node_modules' not in root:
     for file in files:
         if file == 'package.json':
-            print(os.path.join(root, file))
             with open(os.path.join(root, file)) as out:
                 try:
                     packageJson = json.load(out)
                 except json.decoder.JSONDecodeError:
+                    print("could not include ", os.path.join(
+                        root, file), " malformed JSON")
                     continue
-                data[root] = {"version": packageJson.get('version', "no version specified"), "license": packageJson.get('license', "no license"), "dependencies": {
-                    "dependencies": packageJson.get('dependencies', "no dependencies"), "devDependencies": packageJson.get('devDependencies', "no dev-dependencies")}}
+                version = packageJson.get('version', "no version specified")
+                license = packageJson.get('license', "no license")
+                licenseSums.append(str(license))
+                dependencies = packageJson.get(
+                    'dependencies', "no dependencies")
+                devDependencies = packageJson.get(
+                    'devDependencies', "no dev-dependencies")
 
+                data[root] = {"version": version, "license": license, "dependencies": {
+                    "dependencies": dependencies, "devDependencies": devDependencies}}
+
+print(licenseSums)
+licenseSums = collections.Counter(licenseSums)
+
+with open(outputLicenseSums, 'w') as out:
+    out.write(json.dumps(licenseSums))
 
 with open(output, 'w') as out:
     out.write(json.dumps(data))
-print('wrote to ', output)
+print('----')
+print('wrote results to ', output)
+print('wrote license counts to ', outputLicenseSums)
 print("run 'npm install' beforehand to get a full list, otherwise only top-level packages will be analyzed.")
